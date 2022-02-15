326a2d085afb999effd5f2229d46eaca6fe4650e
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2022-02-15 00:12:40 -0600

    WIP

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,76 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', (req, res, next) =>
+{
+    try
+    {
+        throw new Error('Ouch!!!');
+    } catch (err)
+    {
+        next(err);
+    }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get('/:id', (req, res, next) =>
+{
+    try
+    {
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-})
+    } catch (err)
+    {
+        next(err);
+    }
+});
+
+router.post('/', (req, res, next) =>
+{
+    try
+    {
+
+    } catch (err)
+    {
+        next(err);
+    }
+});
+
+router.put('/:id', (req, res, next) =>
+{
+    try
+    {
+
+    } catch (err)
+    {
+        next(err);
+    }
+});
+
+router.delete('/:id', (req, res, next) =>
+{
+    try
+    {
+
+    } catch (err)
+    {
+        next(err);
+    }
+});
+
+router.use((err, req, res, next) =>
+{ // eslint-disable-line
+    try
+    {
+
+    } catch (err)
+    {
+        next(err);
+    }
+});
+
+router.use((err, req, res, next) =>
+{
+    res.status(err.status || 500).json({
+        message: err.message
+    });
+});
 
 module.exports = router;
